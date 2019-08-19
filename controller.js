5b9f6667d7fcced0fc8c3656cf0237a848531a26
Harshil J <harshiljasra@gmail.com>
2019-08-19 18:29:41 -0400

    Extracted Form Values

--- controller.js
+++ controller.js
@@ -1,5 +1,21 @@
 class ExpenseController{
   constructor(view){
-    console.log('controller')
+    this.DOM = view.getDOM();
+
+    this.setUpEventHandlers();
+  }
+
+  setUpEventHandlers(){
+    this.DOM.expenseForm.addEventListener('submit', this.addExpense);
+  }
+
+  addExpense(e){
+    e.preventDefault();
+    const form = e.currentTarget;
+    const amount = form.amount.value;
+    const date = form.date.value;
+    const description = form.description.value;
+
+    console.log (amount, date, description);
   }
 }
